3e90128a714a39396a1d6175f367c3987cc2f0d2
unknown <sony@.(none)>
2014-09-04 20:49:55 +0200

    1

--- application/config/oauth2.php
+++ application/config/oauth2.php
@@ -5,6 +5,7 @@ $config['oauth2']['weibo'] = array(
 	'id' => '3016759760',
 	'secret' => 'b9ed361b3498557385462add26975b71',
 	'callback'  => 'http://partyus.wicp.net/index/sinacallback',
+	'redirect_uri_key' => 'http://partyus.zhang-wei.me/index/sinacallback',
         'extra' => ''
 );
 ?>
