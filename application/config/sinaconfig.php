f6c3589aa79843b427c039f8e2295299eee881fd
unknown <sony@.(none)>
2014-09-04 21:44:30 +0200

    1

--- application/config/sinaconfig.php
+++ application/config/sinaconfig.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '3016759760' );
 define( "WB_SKEY" , 'b9ed361b3498557385462add26975b71' );
-define( "WB_CALLBACK_URL" , 'http://partyus.wicp.net/index/sina_callback' );
+define( "WB_CALLBACK_URL" , 'http://partyus.zhang-wei.me/index/sina_callback' );
--- application/views/login_index.php
+++ application/views/login_index.php
@@ -1,5 +1,10 @@
 <?	$this->load->view('inc/header_view') ;
+    $this->load->library('saetoauth2') ;
+	$this->load->config('sinaconfig') ;
 
+$o = $this->saetoauth2->SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+$code_url = $this->saetoauth2->getAuthorizeURL( WB_CALLBACK_URL );
 ?>
 <body>
 <? $this->load->view('inc/nav_view') ?>
@@ -52,7 +57,7 @@
           <legend class="">快速通过社交帐号登录</legend>
         </div>
         <div id="legend" class="">
-          <a href="http://23.89.232.77/sns/session/weibo"><img src="<?=base_url('resource/images/sinaweibo.gif') ?>"></a>
+          <a href=<?=$code_url?><img src="<?=base_url('resource/images/sinaweibo.gif') ?>"></a>
           
         </div>
     </div>
