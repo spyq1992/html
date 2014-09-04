28cbcb81275409083978f27ea1b303f83238db23
unknown <sony@.(none)>
2014-09-04 23:34:47 +0200

    1

--- application/config/sinaconfig.php
+++ application/config/sinaconfig.php
@@ -1,6 +1,6 @@
 <?php
 
 
-define( "WB_AKEY" , '3016759760' );
-define( "WB_SKEY" , 'b9ed361b3498557385462add26975b71' );
-define( "WB_CALLBACK_URL" , 'http://partyus.zhang-wei.me/index/sina_callback' );
+$config['WB_AKEY']	= '3016759760';
+$config['WB_SKEY']	= 'b9ed361b3498557385462add26975b71';
+$config['WB_CALLBACK_URL']	= 'http://partyus.zhang-wei.me/index/sina_callback';
