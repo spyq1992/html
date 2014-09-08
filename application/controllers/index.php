867914a10ad202d4cc9b908b59d2f498c9c141bd
unknown <sony@.(none)>
2014-09-08 23:54:37 +0200

    1

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -158,9 +158,10 @@ class Index extends CI_Controller {
 				$keys = array();
 				$keys['code'] = $_REQUEST['code'];
 				$keys['redirect_uri'] = WB_CALLBACK_URL;
+				$keys['refresh_token']= null;
 				try {
 					echo "1";
-					$token = $o->getAccessToken( 'token', $keys ) ; ;
+					$token = $o->getAccessToken( 'code', $keys ) ; ;
 					echo "2";
 					$_SESSION['token'] = $token;
 					setcookie( 'weibojs_'.$this->saetoauthv->client_id, http_build_query($token) );	
