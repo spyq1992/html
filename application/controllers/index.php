5b1541ad4846d470b880a5049807bb3029ee64b8
unknown <sony@.(none)>
2014-09-15 13:42:21 +0200

    1

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -162,7 +162,7 @@ class Index extends CI_Controller {
 				$keys['refresh_token']= null;
 				
 				$uid= array();
-				$this->saetoauthv->get('account/get_uid',$uid);
+				$this->saetoauthv->get('2/account/get uid',$uid);
 				var_dump($uid);
 				if($this->Weibo_model->exist_uid($uid['uid'])){
 				try {
