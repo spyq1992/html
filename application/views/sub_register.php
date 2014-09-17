922c746c36965d579cc437fe57547e1d127f64f4
unknown <sony@.(none)>
2014-09-17 19:22:26 +0200

    1

--- application/views/sub_register.php
+++ application/views/sub_register.php
@@ -15,7 +15,7 @@
           <!-- Text input-->
           <label class="control-label" for="input01">邮箱</label>
           <div class="controls">
-            <input type="text" placeholder="您的邮箱地址" class="input-xlarge" id="email" name="email">
+            <input type="text" placeholder="您的邮箱地址" class="input-xlarge" id="user_email" name="user_email">
             <p class="help-block"></p>
             <input type="hidden" id="email_code">
             <span class="prompt" id="email_prompt"></span>
@@ -28,7 +28,7 @@
           <!-- Text input-->
           <label class="control-label" for="input01">密码</label>
           <div class="controls">
-            <input type="password" placeholder="您的密码" class="input-xlarge" maxlength="20" id="pwd" name="pwd">
+            <input type="password" placeholder="您的密码" class="input-xlarge" maxlength="20" id="user_pwd" name="user_pwd">
             <span class="prompt" id="pwd_prompt"></span>
           </div>
         </div>
@@ -37,17 +37,21 @@
     </div>
     </fieldset>
  
+     <?=form_close() ?>
 
-<?=form_close() ?>
- <div class="span4">
+    </div>
+    <div class="span4">
       <div id="legend" class="">
           <legend class="">快速通过社交帐号登录</legend>
         </div>
         <div id="legend" class="">
-          <a href="<?=base_url('index/loginWithWeibo')?>"><img src="<?=base_url('resource/images/sinaweibo.gif') ?>"></a>
-          <br><br>
-          <a href=""><img src="<?=base_url('resource/images/douban.png') ?>"></a>
-          <legend class="" style="margin-top:20px"></legend>
+          <a href="<?=base_url('index/loginWithWeibo')?>" > <img src="<?=base_url('resource/images/sinaweibo.gif') ?>"></a>
+          
+        </div>
+    </div>
+  </div>
+</div>
+  <legend class="" style="margin-top:20px"></legend>
           已有帐号？
           <button type="submit" class="btn btn-primary" type="button">登录</button>
           
